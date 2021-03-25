14cd1c4adb6a37b9f93e53b350d8f322f573c419
Strermko <znatokistoriy@gmail.com>
2021-03-25 11:31:18 +0100

    lab2

---
+++ Lab_2/src/app.ts
@@ -0,0 +1,9 @@
+import Snake from "./Snake";
+import Horse from "./Horse";
+import Animal from "./Animal";
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Polomino");
+
+sam.move();
+tom.move();
